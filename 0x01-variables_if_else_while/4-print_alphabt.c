9160047804eae1562f63b88bba90ca05726bd8ab
Gladys <gladys.okpe@gmail.com>
2022-03-18 01:47:05 -0700

    ,

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+able File  20 lines (17 sloc)  304 Bytes
+   
+#include <stdio.h>
+
+/**
+* main - Print the alphabet in lowercase letters, except for e and q
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		if (letter != 'e' && letter != 'q')
+		putchar(letter);
+		}
+		putchar('\n');
+
+	return (0);
+}
